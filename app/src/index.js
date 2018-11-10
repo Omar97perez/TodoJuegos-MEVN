1022eff2e7c6d459da5f40d2ab4ee6401a8acd79
Juan Jesús Padrón Hernández <alu0100976731@ull.edu.es>
2018-11-10 13:11:49 +0000

    Añadiendo rutas para elementos estátidos. Reubicando index.html a src/public

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+
+// static
+app.use(express.static(path.join(__dirname, 'public')));;
 
 app.listen(app.get('port'), () => {
     console.log(`server listening on port ${app.get('port')}`);

a4f498a5379c263f90c9a3c27240e05d46570a0a
SkyZyy <sergito1997.sd@gmail.com>
2018-11-18 12:08:41 +0000

    Carrito de la compra[Front end]

--- app/src/index.js
+++ app/src/index.js
@@ -32,42 +32,8 @@ mongoose.connect(remoteUrl, (err,res) => {
 }
 });
 
-
 /*  JS Carrito */
-$('.minus-btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
 
-    if (value > 1) {
-        value = value - 1;
-    } else {
-        value = 0;
-    }
-
-$input.val(value);
-
-});
-
-$('.plus-btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
-
-    if (value < 100) {
-      value = value + 1;
-    } else {
-        value =100;
-    }
-
-    $input.val(value);
-});
-
-$('.like-btn').on('click', function() {
-$(this).toggleClass('is-active');
-});
 // app.listen(app.get('port'), () => {
 //     console.log(`server listening on port ${app.get('port')}`);
 // })

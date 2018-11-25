6ecdd7a25512a82fd43fff5799dd29e7a6394379
juanpabloclaros <juan_pablo_claros@hotmail.com>
2018-11-25 19:54:19 +0000

    Router a componentes creados

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -8,8 +8,43 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'index',
       component: HelloWorld
+    },
+    {
+      path: '/registro',
+      name: 'registro',
+      component: registro
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/carrito',
+      name: 'carrito',
+      component: carrito
+    },
+    {
+      path: '/insertar_producto',
+      name: 'insertar_producto',
+      component: insertar_producto
+    },
+    {
+      path: '/Pc',
+      name: 'Pc',
+      component: Pc
+    },
+    {
+      path: '/Ps4',
+      name: 'Ps4',
+      component: Ps4
+    },
+    {
+      path: '/Xbox_one',
+      name: 'Xbox_one',
+      component: Xbox_one
     }
   ]
 })

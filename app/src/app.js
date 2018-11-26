f4e32fc9f05bda7ae1870b6e8777baf4c8ff9620
Omar97perez <omarperezznakar@gmail.com>
2018-11-26 19:44:33 +0000

    Adaptación Página administrador a proyecto

--- app/src/app.js
+++ app/src/app.js
@@ -34,7 +34,7 @@ const indexRoutes = require('./routes/index');
 
 // settings
 app.set('port', process.env.PORT || 4000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'Public'));
 app.set('view engine', 'ejs');
 
 // middlewares

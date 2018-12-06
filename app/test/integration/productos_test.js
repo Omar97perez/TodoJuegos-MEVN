ed9e6e7934e4b3f2258decfb99a46e5020079571
Sergio <alu0100948097@ull.edu.es>
2018-12-06 19:22:01 +0000

    Reparando tests

--- app/test/integration/productos_test.js
+++ app/test/integration/productos_test.js
@@ -4,33 +4,33 @@ const productos_js  = require("../../src/models/Productos.js");
 
 describe("Pruebas sobre Productos.js", function(){
     it("comprobando existe objeto Productos", function(){
-        assert.typeOf(productos_js.Productos.obj, "object");
+        assert.typeOf(productos_js.schema.obj, "object");
     });
     it("comprobando que campo id es de tipo Number", function(){
-        assert.equal(productos_js.Productos.paths.id.instance, "Number");
+        assert.equal(productos_js.schema.paths.id.instance, "Number");
     });
     it("comprobando que campo titulo es de tipo String", function(){
-        assert.equal(productos_js.Productos.paths.titulo.instance, "String");
+        assert.equal(productos_js.schema.paths.titulo.instance, "String");
     });
     it("comprobando que campo foto es de tipo String", function(){
-        assert.equal(productos_js.Productos.paths.foto.instance, "String");
+        assert.equal(productos_js.schema.paths.foto.instance, "String");
     });
     it("comprobando que campo descripcion es de tipo String", function(){
-        assert.equal(productos_js.Productos.paths.descripcion.instance, "String");
+        assert.equal(productos_js.schema.paths.descripcion.instance, "String");
     });
     it("comprobando que campo tipo es de tipo String", function(){
-        assert.equal(productos_js.Productos.paths.tipo.instance, "String");
+        assert.equal(productos_js.schema.paths.tipo.instance, "String");
     });
     it("comprobando que campo plataforma es de tipo String", function(){
-        assert.equal(productos_js.Productos.paths.plataforma.instance, "String");
+        assert.equal(productos_js.schema.paths.plataforma.instance, "String");
     });
     it("comprobando que campo categoria es de tipo String", function(){
-        assert.equal(productos_js.Productos.paths.categoria.instance, "String");
+        assert.equal(productos_js.schema.paths.categoria.instance, "String");
     });
     it("comprobando que campo precio es de tipo Number", function(){
-        assert.equal(productos_js.Productos.paths.precio.instance, "Number");
+        assert.equal(productos_js.schema.paths.precio.instance, "Number");
     });
     it("comprobando que campo oferta es de tipo Number", function(){
-        assert.equal(productos_js.Productos.paths.oferta.instance, "Number");
+        assert.equal(productos_js.schema.paths.oferta.instance, "Number");
     });
 });

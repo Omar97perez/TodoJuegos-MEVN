b709a41bdb019d00f7d713feb242de16f163478f
SkyZyy <sergito1997.sd@gmail.com>
2018-12-25 19:37:07 +0000

    Resolviendo fallos de botones del slider

--- app/src/app/components/Inicio.vue
+++ app/src/app/components/Inicio.vue
@@ -117,10 +117,10 @@
 
         </div>
         <!-- Control del carousel -->
-        <a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
+        <a class="carousel-control left carousel-control-prev" href="#carouselpc" data-slide="prev">
           <i class="fa fa-angle-left"></i>
         </a>
-        <a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
+        <a class="carousel-control right carousel-control-next" href="#carouselpc" data-slide="next">
           <i class="fa fa-angle-right"></i>
         </a>
       </div><!-- Final carousel -->
@@ -175,10 +175,10 @@
         </div>
 
         <!-- Control del carousel -->
-        <a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
+        <a class="carousel-control left carousel-control-prev" href="#carouselxbox" data-slide="prev">
           <i class="fa fa-angle-left"></i>
         </a>
-        <a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
+        <a class="carousel-control right carousel-control-next" href="#carouselxbox" data-slide="next">
           <i class="fa fa-angle-right"></i>
         </a>
       </div><!-- Final carousel -->
